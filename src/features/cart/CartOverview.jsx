4658a5e2a218662743ab428c38b646d9161e8dc9
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-06-16 16:17:13 +0300

    Enhance CartOverview component with styled-components for improved styling

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 function CartOverview() {
   return (
-    <div>
-      CartOverview
+    <Wrapper>
+      <Info>
+        <span>23piza</span>
+        <span>$23.45</span>
+      </Info>
       <Link to='/cart'>go to cart</Link>
-    </div>
+    </Wrapper>
   );
 }
 
 export default CartOverview;
+
+const Wrapper = styled.div`
+  background-color: #292524;
+  color: #d6d3d1;
+  padding-left: 1rem;
+  text-transform: uppercase;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  justify-content: space-between;
+`;
+
+const Info = styled.p`
+  display: flex;
+  gap: 1rem;
+`;
