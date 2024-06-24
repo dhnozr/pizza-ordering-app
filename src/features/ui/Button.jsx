a211348e3723a4be4ef2e217b307f77a3cfd84ce
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-06-24 11:57:52 +0300

    Refactor Button component to handle secondary prop correctly
    - Updated background color, outline, font weight, and hover state to use the correct secondary prop in styled-components.
    - Ensured that all button styles are correctly applied based on the secondary prop for consistent theming and appearance.

--- src/features/ui/Button.jsx
+++ src/features/ui/Button.jsx
@@ -14,8 +14,8 @@ export default Button;
 const Btn = styled.button`
   display: inline-block;
   border: none;
-  background-color: ${props => (props.secondary ? 'transparent' : '#facc15')};
-  outline: ${props => (props.secondary ? '1px solid #e7e5e4' : null)};
+  background-color: ${props => (props.$secondary ? 'transparent' : '#facc15')};
+  outline: ${props => (props.$secondary ? '1px solid #e7e5e4' : null)};
   text-transform: ${props => (props.type === 'small' ? null : 'uppercase')};
   color: #292524;
   padding: ${props => (props.type === 'small' ? '3px 12px' : '8px 22px')};
@@ -24,14 +24,14 @@ const Btn = styled.button`
   transition: all 0.3s linear;
   text-decoration: none;
   font-size: 14px;
-  font-weight: ${props => (props.secondary ? '700' : null)};
+  font-weight: ${props => (props.$secondary ? '700' : null)};
 
   &:disabled {
     opacity: 0.5;
   }
 
   &:hover {
-    background-color: ${props => (props.secondary ? '#e5d586' : '#fde047')};
+    background-color: ${props => (props.$secondary ? '#e5d586' : '#fde047')};
   }
 
   @media screen and (min-width: 660px) {
