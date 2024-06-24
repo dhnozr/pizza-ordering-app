b49cfba07ec496f4bd8b505679737b2f28bc2867
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-06-24 11:47:59 +0300

    Refactor CartOverview component to use Redux state management for cart details
    - Added import statements for useSelector, getTotalCartQuantity, and getTotalCartPrice from Redux slice.
    - Removed hardcoded total pizza quantity and total cart price values.
    - Used useSelector to retrieve total cart quantity and total cart price from Redux state.
    - Added conditional rendering to return null if the cart is empty.

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { getCart, getTotalCartPrice, getTotalCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
   return (
     <Wrapper>
       <Info>
-        <span>23piza</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </Info>
       <Link to='/cart'>go to cart</Link>
     </Wrapper>
